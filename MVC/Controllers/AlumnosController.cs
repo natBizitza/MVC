f6d6ab322de5df5aa394822082134caf1de15881
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-11-13 15:35:55 +0100

    Add project files.

---
+++ MVC/Controllers/AlumnosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        public IActionResult Index()
+        {
+            Alumno a1 = new Alumno
+            {
+                Id = 1,
+                Nombre="Natalia",
+                Apellido="Romankevich"
+            };
+            return View(a1);
+        }
+    }
+}
